fc8833792e6e701908dd69748bc2edb778846079
JabulaniMcineka <Jabulani.Mcineka@ahri.org>
2026-06-21 01:16:14 +0200

    Add Day 28, Day 29, and Day 30 solutions

---
+++ Day 30/main.py
@@ -0,0 +1,75 @@
+# #fileNotFound
+
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["non_existent_key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:    
+#         content = file.read()
+#         print(content)
+# finally:    
+#         raise TypeError("This is an error that I made up.")
+#         file.close()
+#         print("File was closed.")  
+
+# raise TypeError("This is an error that I made up.")
+
+
+# height = float(input("Height: "))
+# weight = int(input("Weight: "))         
+
+# if height < 3:
+#     raise ValueError("Human height should not be over 3 meters.")
+
+# bmi = weight / height ** 2
+# print(bmi)
+
+
+
+#Exercise
+
+# fruits = ["Apple", "Pear", "Orange"]
+
+# # Catch the exception and make sure the code runs without crashing.
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("Fruit index out of range.")
+#     else:
+#         print(fruit + " pie")
+
+# make_pie(4)
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+
+    total_likes = 0
+    for post in posts:
+        total_likes = total_likes + post.get('Likes', 0)
+    
+    return total_likes
+
+try:
+    count_likes(facebook_posts)
+except KeyError as error_message:
+    print(f"Key {error_message} not found.")
+
+
+
+
+
